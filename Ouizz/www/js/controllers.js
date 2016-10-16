1f44b384717a9590066eefc53f503e0be1c467fd
ggrelet <guillaume.grelet@gmail.com>
2016-10-16 18:51:37 +0200

    WIP

--- Ouizz/www/js/controllers.js
+++ Ouizz/www/js/controllers.js
@@ -1,15 +1,22 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Evenements) {
-    $scope.evenements = Evenements.all();
-  $scope.remove = function(evenement) {
-    Evenements.remove(evenement);
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.events = Events.all();
+  $scope.remove = function(event) {
+    Events.remove(event);
   };
 })
 
-.controller('EvenementsCtrl', function($scope, Evenements) {
-
-
-.controller('EvenementDetailCtrl', function($scope, $stateParams, Evenements) {
-  $scope.evenement = Evenements.get($stateParams.evenementId);
+.controller('EventDetailCtrl', function($scope, $stateParams, Events) {
+  $scope.event = Events.get($stateParams.eventId);
 });
