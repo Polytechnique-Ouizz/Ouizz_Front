9919ab4ed146da24b9b9daddf5218beee12e1ff5
ggrelet <guillaume.grelet@gmail.com>
2016-11-10 11:55:40 +0100

    Modification de master

--- Ouizz/www/js/app.js
+++ Ouizz/www/js/app.js
@@ -58,7 +58,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'EventDetailCtrl'
         }
       }
-    }
     });
 
   // if none of the above states are matched, use this as the fallback
